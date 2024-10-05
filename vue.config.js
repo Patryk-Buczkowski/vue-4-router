72eac8a1d8c24c6e33e2d425f45b9a16634b3e6e
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-10-05 15:23:57 +0200

    add base

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+    base: '/vue-4-router'/,
     devServer: {
       proxy: {
         '/api': {
           target: 'https://travel-dummy-api.netlify.app/',
           changeOrigin: true,
-          pathRewrite: { '^/api': '' }, // opcjonalne
+          pathRewrite: { '^/api': '' },
         },
       },
     },
