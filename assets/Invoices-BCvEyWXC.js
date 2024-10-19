2959ce1132dd17b2cbad49749772de22c12c8b64
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-10-19 08:36:55 +0200

    'Deploy'

---
+++ assets/Invoices-BCvEyWXC.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as s,b as t,o as r}from"./index-biuabRlY.js";const c={};function n(a,e){return r(),s("div",null,e[0]||(e[0]=[t("h1",{class:"text-blue-700"},"invoices",-1)]))}const f=o(c,[["render",n]]);export{f as default};
