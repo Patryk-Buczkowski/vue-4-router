655cf0a72a2e5a7c0b302a865595371e19cef1eb
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-10-19 16:45:07 +0200

    'Deploy'

---
+++ assets/Invoices-C4jNess5.js
@@ -0,0 +1,1 @@
+import{j as r,c as s,b as t,o as n}from"./index-BDUMOR8S.js";const u={__name:"Invoices",setup(a){return r((o,e)=>{if(!window.confirm("Are You sure to leave?"))return!1}),(o,e)=>(n(),s("div",null,e[0]||(e[0]=[t("h1",{class:"text-blue-700"},"invoices",-1)])))}};export{u as default};
